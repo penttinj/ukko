33b06dd2eceef30f3f22da8a0e57707836839de7
Johan Penttinen <johanlepenttinen@gmail.com>
2021-10-07 13:17:58 +0300

    Change to a nicer snowflake

--- server/static/interface.js
+++ server/static/interface.js
@@ -61,7 +61,7 @@ const createTempCard = async (reading, id) => {
     temp.appendChild(document.createElement("br"));
     hum.appendChild(document.createTextNode(reading.values.humidity ? `💦${reading.values.humidity}%` : "💦 -"));
     hum.appendChild(document.createElement("br"));
-    feels.appendChild(document.createTextNode(`❄4.20C`));
+    feels.appendChild(document.createTextNode(`❄️4.20C`));
     feels.appendChild(document.createElement("br"));
     feels.appendChild(document.createTextNode(`🔥69.00C`));
 
