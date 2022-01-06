fd8b4af5d627f669ecd6371fb8bb53896d990a29
Johan Penttinen <johanlepenttinen@gmail.com>
2022-01-06 23:17:22 +0200

    Get started with material ui

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@mui/material/styles';
 import './index.css';
 import App from './App';
+import theme from "./theme";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
