7b1ae141da578fdb864931f13b86227c3aa4c513
kaibiprime <johanlepenttinen@gmail.com>
2022-01-10 20:17:24 +0200

    experiment with grid

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Button } from "@mui/material"
-import { Container } from '@mui/material';
+import Grid from '@mui/material/Grid';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props: any) {
+  console.log("props=", props)
+  
   return (
-    <Container>
-      <Button variant="contained" color="primary">
-        Sup
-      </Button>
-    </Container>
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        Shoop
+      </Grid>
+      <Grid item xs={4}>
+        Da
+      </Grid>
+      <Grid item xs={6}>
+        Whoop
+      </Grid>
+      <Grid item xs={6}>
+        Whoop
+      </Grid>
+    </Grid>
   );
 }
 
