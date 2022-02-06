e346e85feadebe38839348584f050848c7124c99
Johan Penttinen <johanlepenttinen@gmail.com>
2022-02-06 21:05:27 +0200

    Get requests for individual sensor data

--- server/server.py
+++ server/server.py
@@ -14,7 +14,7 @@ import os
 
 config = {"DEBUG": True}  # some Flask specific configs
 # Global variable holding all the sensor data
-sensors = dict()
+sensor_data = dict()
 
 
 app = flask.Flask(__name__)
@@ -23,7 +23,7 @@ CORS(app)
 
 @app.route("/")
 def root():
-    return render_template("index.html", message=sensors)
+    return render_template("index.html", message=sensor_data)
 
 
 @app.route("/favicon.ico")
@@ -35,39 +35,44 @@ def serve_favicon():
 def parse_data():
     data: dict = request.get_json()
     node_name = data["node_name"]
-    if node_name not in sensors:
-        print(f"sensors[{node_name}] was not in sensors dict")
-        sensors[node_name] = dict()
-        sensors[node_name]["current_day"] = time.localtime()[2]
-    elif sensors[node_name]["current_day"] != time.localtime()[2]:
+    if node_name not in sensor_data:
+        print(f"sensor_data[{node_name}] was not in sensor_data dict")
+        sensor_data[node_name] = dict()
+        sensor_data[node_name]["current_day"] = time.localtime()[2]
+    elif sensor_data[node_name]["current_day"] != time.localtime()[2]:
         # Is there a smarter way to do this? If i add an `or` to the previous if statement, there'll
         # be a KeyError.
-        print(f"New day for sensors[{node_name}]")
-        sensors[node_name] = dict()
-        sensors[node_name]["current_day"] = time.localtime()[2]
+        print(f"New day for sensor_data[{node_name}]")
+        sensor_data[node_name] = dict()
+        sensor_data[node_name]["current_day"] = time.localtime()[2]
     for key, value in data.items():
-        sensors[node_name][key] = value
-    sensors["updated"] = time.asctime()
-    if ("min" not in sensors[node_name]):
-        print(f"min not in {sensors[node_name]}", "min" not in sensors[node_name])
-        sensors[node_name]["min"] = data["temperature"]
-    if ("max" not in sensors[node_name]):
-        print(f"max not in {sensors[node_name]}", "max" not in sensors[node_name])
-        sensors[node_name]["max"] = data["temperature"]
+        sensor_data[node_name][key] = value
+    sensor_data["updated"] = time.asctime()
+    if "min" not in sensor_data[node_name]:
+        print(f"min not in {sensor_data[node_name]}", "min" not in sensor_data[node_name])
+        sensor_data[node_name]["min"] = data["temperature"]
+    if "max" not in sensor_data[node_name]:
+        print(f"max not in {sensor_data[node_name]}", "max" not in sensor_data[node_name])
+        sensor_data[node_name]["max"] = data["temperature"]
 
-    if sensors[node_name]["min"] > data["temperature"]:
-        sensors[node_name]["min"] = data["temperature"]
-    
-    if sensors[node_name]["max"] < data["temperature"]:
-        sensors[node_name]["max"] = data["temperature"]
-    
-    print("New sensors dict:", sensors)
+    if sensor_data[node_name]["min"] > data["temperature"]:
+        sensor_data[node_name]["min"] = data["temperature"]
+
+    if sensor_data[node_name]["max"] < data["temperature"]:
+        sensor_data[node_name]["max"] = data["temperature"]
+
+    print("New sensors dict:", sensor_data)
     return jsonify(data), 200
 
+@app.route("/api/v1/sensors/<sensor>")
+def get_sensor(sensor):
+    if sensor not in sensor_data:
+        return f"Sensor {sensor} not found", 404
+    return jsonify(sensor_data[sensor]), 200
 
 @app.route("/api/v1/sensors", methods=["GET"])
 def get_readings():
-    return jsonify(sensors), 200
+    return jsonify(sensor_data), 200
 
 
 @app.after_request
